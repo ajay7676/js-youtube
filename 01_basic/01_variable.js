dea0e9f39171639a51a9a4a3c203bd9ab7c34f68
ajay7676 <62447178+ajay7676@users.noreply.github.com>
2023-08-29 01:29:33 +0000

    01_variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let  accEmail ="ajay@gmail.com";
+var password = "12345";
+accountCity= "Noida";
+
+console.log(accountId);
+console.log(accEmail);
+console.log(password);
+console.log(accountCity);
+
+
+accEmail = "asdf@gmail.com"
+password = "55555";
+accountCity = "Jaipur";
+console.table([accountId,accEmail, password , accountCity])
