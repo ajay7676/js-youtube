cd8a63ac99632884a05384247105bb1a2bd91d04
ajay7676 <62447178+ajay7676@users.noreply.github.com>
2023-08-31 17:52:36 +0000

    datatype

---
+++ 02_datatype/index.js
@@ -0,0 +1,15 @@
+ //primtive Data type
+
+// number => 2 to 53
+ // bigInt
+ // string  => " "
+ // boolean => true /false
+ // null => standlone value (null a type )
+ // undefined => (whenever we are not intailzed value ) 
+// symbol  =>  whenevener we neee to unique
+
+// NON PRIMITIVE TYPE
+
+console.log(typeof "ajay")
+console.log(typeof undefined);
+console.log(typeof null)
