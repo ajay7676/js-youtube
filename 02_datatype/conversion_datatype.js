38ca09fe0afe0144cb55504c2a08c3a0d704a7f6
ajay7676 <62447178+ajay7676@users.noreply.github.com>
2023-08-31 18:08:00 +0000

    datatypeconversion

---
+++ 02_datatype/conversion_datatype.js
@@ -0,0 +1,11 @@
+// let score = "ajaychsuhan";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+let isLoggedIn = 1
+let booleanLogged  = Boolean (isLoggedIn);
+console.log(booleanLogged);
