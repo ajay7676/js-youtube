e7db23d149491db467a999115a31be036774d091
ajay7676 <62447178+ajay7676@users.noreply.github.com>
2024-11-27 05:54:12 +0000

    Complete JS Basic

---
+++ data-time/index.js
@@ -0,0 +1,21 @@
+// console.log("Data & Time");
+
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toDateString);
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreateDate = new Date (2024 , 10 , 27)
+//  console.log(myCreateDate.toDateString());
+ 
+// let myCreateDate = new Date (2024 , 10 , 27 , 5 , 3)
+let myCreateDate = new Date ("01-14-2024")
+//  console.log(myCreateDate.toLocaleString());
+ 
+
+let myTimeStamp = Date.now();
+ console.log(myTimeStamp);
+ 
